90acf59cdd2a8f30e4690aac02d3cfc464c0308e
DESKTOP-KNEF2HR <stranniknochi@mail.ru>
2022-12-10 18:14:10 +0800

    first commit

---
+++ Zadacha_19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3]){ //если 1-я цифра = 5-й цифре и 2-я цифра = 4-й цифре то
+    Console.WriteLine("Число является палиндромом: " + number + " -> Да!"); // число Палиндром!
+  }
+  else Console.WriteLine("Число является палиндромом: " + number + " -> Нет!");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine("Число не является палиндромом");
---
+++ Zadacha_21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// // A(x,y,z)
+
+// AB = √(x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2
+
+Console.Write("Введите координату х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+// Math.Sqrt(число) => Math.Sqrt(25) => 5
+// Math.Pow (2, 3) => 8 = 2 * 2 * 2
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)); // Math.Pow(число, степень)
+// Math.Round(число, количество знаков) 
+Console.WriteLine($"Расстояние между 2 точками: {x1},{y1},{z1}  {x2},{y2},{z2} = {Math.Round(d,3)}"); // Интерполяция
